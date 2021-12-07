d9eacdc9963eca46cc3a243fc3688f9ce73e99a4
ohmnapt <napats.work@gmail.com>
2021-12-08 02:36:18 +0700

    add css with js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,125 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button type="button" @click="flag = !flag">Toggle</button>
+
+    <!-- <transition name="fade" mode="out-in">
+      <h2 v-if="flag" key="main">Hello world</h2>
+      <h2 v-else key="secondary">Another hello</h2>
+    </transition> -->
+
+    <!-- <transition name="zoom" type="animation" appear>
+      <h2 v-if="flag">Hello</h2>
+    </transition> -->
+
+    <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      :css="false"
+    >
+      <h2 v-if="flag">Hey</h2>
+    </transition>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      flag: true,
+    };
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log("before-enter event fired", el);
+    },
+    enter(el, done) {
+      console.log("enter event fired", el);
+      const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
+        duration: 1000,
+      });
+      animation.onfinish = () => {
+        done();
+      };
+    },
+    afterEnter(el) {
+      console.log("after-enter event fired", el);
+    },
+    beforeLeave(el) {
+      console.log("before-leave event fired", el);
+    },
+    leave(el, done) {
+      console.log("leave event fired", el);
+      const animation = el.animate([{}, { transform: "scale3d(0,0,0)" }], {
+        duration: 1000,
+      });
+      animation.onfinish = () => {
+        done();
+      };
+    },
+    afterLeave(el) {
+      console.log("after-leaeve event fired", el);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 1s linear;
+}
+
+.fade-leave-to {
+  transition: all 1s linear;
+  opacity: 0;
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 2s linear;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 2s linear;
+}
+
+.zoom-enter-from {
+  opacity: 0;
+}
+
+.zoom-leave-to {
+  opacity: 0;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 </style>
